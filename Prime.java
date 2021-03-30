2922b37568b8adcf15e7746b81072b15983a2d0a
ariel <71622992+ariiel1@users.noreply.github.com>
2021-03-30 23:05:22 +0700

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,22 @@
+package Prime;
+
+public class Prime {
+
+	public static void main(String[] args) {
+	
+	boolean isPrime = true;	  
+	int num = 14;
+	
+	for (int i = 2; i <= num / 2; ++i) {
+		if (num % i == 0) {
+			isPrime = false;
+			break;
+		}
+	}
+
+	if (isPrime)
+		System.out.println(num + " is a prime number");
+	else
+	    System.out.println(num + " is not a prime number");
+	  }
+}
